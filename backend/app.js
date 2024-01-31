3f307544b96c84082afa8a1dbeadae3727c7a66b
salman <salman.iqureshi04@gmail.com>
2024-01-31 10:08:11 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,92 @@
+var createError = require('http-errors');
+var express = require('express');
+const { connecting } = require('./config/connecting');
+connecting()
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors= require('cors')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter=require('./routes/login')
+var uploadRouter=require('./routes/Upload')
+var searchRouter=require('./routes/Search')
+var messageRouter=require('./routes/Message')
+
+var app = express();
+
+
+app.use(cors())
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/users', usersRouter);
+app.use('/upload', uploadRouter);
+app.use('/search', searchRouter);
+app.use('/message', messageRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+const server = app.listen('8000',()=>{
+  console.log('server started');
+})
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
